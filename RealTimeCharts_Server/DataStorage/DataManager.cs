3237f6701d1e679c9a4ca865b2619ae3f8109bc9
Wanasak <u510610433@gmail.com>
2018-12-13 01:14:01 +0700

    timer implementation with datamanager and chartcontroller

---
+++ RealTimeCharts_Server/DataStorage/DataManager.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using RealTimeCharts_Server.Models;
+
+namespace RealTimeCharts_Server.DataStorage
+{
+    public class DataManager
+    {
+        public static List<ChartModel> GetData()
+        {
+            var r = new Random();
+
+            return new List<ChartModel>()
+            {
+                new ChartModel { Data = new List<int> { r.Next(1, 40) }, Label = "Data1" },
+                new ChartModel { Data = new List<int> { r.Next(1, 40) }, Label = "Data2" },
+                new ChartModel { Data = new List<int> { r.Next(1, 40) }, Label = "Data3" },
+                new ChartModel { Data = new List<int> { r.Next(1, 40) }, Label = "Data4" }
+            };
+        }
+    }
+}
