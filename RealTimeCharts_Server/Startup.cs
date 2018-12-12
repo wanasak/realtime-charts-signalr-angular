574098618cbda60e611643deaf06b70083121441
Wanasak <u510610433@gmail.com>
2018-12-13 00:48:47 +0700

    basic configuration

--- RealTimeCharts_Server/Startup.cs
+++ RealTimeCharts_Server/Startup.cs
@@ -25,6 +25,16 @@ namespace RealTimeCharts_Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials()
+                    );
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -41,6 +51,8 @@ namespace RealTimeCharts_Server
             }
 
             app.UseHttpsRedirection();
+            app.UseCors();
+
             app.UseMvc();
         }
     }
